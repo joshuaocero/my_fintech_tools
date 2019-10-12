f2469f34f4be6ac289f1f2399192893e1ecf8638
Joshua Ocero <joshuaocero@yahoo.com>
2019-10-12 12:15:28 +0300

    Setting up Django

---
+++ my_momo_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyMomoAppConfig(AppConfig):
+    name = 'my_momo_app'
---
+++ my_momo_app/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+class MomoRequest(models.Model):
