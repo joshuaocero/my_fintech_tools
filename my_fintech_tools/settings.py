a797ac6939e9ac85b32f277e3977da925b68883e
Joshua Ocero <joshuaocero@yahoo.com>
2019-10-13 00:01:09 +0300

    Implemented calls to MoMo API with mtnmomo library

--- my_fintech_tools/settings.py
+++ my_fintech_tools/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_momo_app',
 ]
 
 MIDDLEWARE = [
@@ -93,3 +94,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Celery settings
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
