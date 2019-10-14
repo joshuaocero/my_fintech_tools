1374c5ff86acc177853da633218f9949b86bcff9
Joshua Ocero <joshuaocero@yahoo.com>
2019-10-14 21:03:57 +0300

    Fixed a minor indentation issue in the signals.py file

--- my_momo_app/signals.py
+++ my_momo_app/signals.py
@@ -7,9 +7,9 @@ from my_fintech_tools.celery import collect_funds
 
 @receiver(post_save, sender=MomoRequest)
 def flag_collection_task(sender, instance, **kwargs):
-	"""
-	After a model save event, make an API request to
-	the MoMo API
-	"""
+    """
+    After a model save event, make an API request to
+    the MoMo API
+    """
     collect_funds.delay(instance.id, instance.mobile_no, instance.amount,
                         instance.external_id, instance.payee_note, instance.payee_message, instance.currency)
